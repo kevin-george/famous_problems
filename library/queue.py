55da9b74139a51fa28f85fa746ba62c696cb53bb
Kevin George <kevin.george@protonmail.com>
2017-07-22 21:00:56 -0500

    Modified function names to reflect operation

--- library/queue.py
+++ library/queue.py
@@ -2,13 +2,13 @@ class Queue:
     def __init__(self):
         self.elements = []
 
-    def addQueue(self, item):
+    def enqueue(self, item):
         self.elements.insert(0, item)
 
-    def removeQueue(self):
+    def dequeue(self):
         return self.elements.pop()
 
-    def isEmpty(self):
+    def is_empty(self):
         return len(self.elements) == 0
 
     def size(self):
